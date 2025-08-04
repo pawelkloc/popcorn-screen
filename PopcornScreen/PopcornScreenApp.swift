57e388758eb8c3d77cc72255c12df901810d666b
Paweł Kloc <pawelkloc89@gmail.com>
2025-08-04 19:03:53 +0200

    Initial commit

---
+++ PopcornScreen/PopcornScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopcornScreenApp.swift
+//  PopcornScreen
+//
+//  Created by Paweł Kloc on 04/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PopcornScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# PopcornScreen
+
+An iOS app for browsing and saving your favorite movies, powered by SwiftUI + TMDB API.
+
+Made with 🍿 by Paweł Kloc.
