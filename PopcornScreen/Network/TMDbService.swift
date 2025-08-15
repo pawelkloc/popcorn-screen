8b008498ccc513d2724d44f48dc537e930e80e78
Paweł Kloc <pawelkloc89@gmail.com>
2025-08-15 10:30:45 +0200

    Created fetchPopularMovies function

--- PopcornScreen/Network/TMDbService.swift
+++ PopcornScreen/Network/TMDbService.swift
@@ -16,5 +16,27 @@ struct TMDbService {
     private let baseURL = URL(string: "https://api.themoviedb.org/3")!
     private let bearerToken: String
     
+    init() {
+        guard let token = Bundle.main.object(forInfoDictionaryKey: "TMDB_TOKEN") as? String else {
+            fatalError("TMDB_TOKEN not found in Info.plist")
+        }
+        
+        self.bearerToken = token
+    }
     
+    func fetchPopularMovies(page: Int) async throws -> [Movie] {
+        var components = URLComponents(url: baseURL.appendingPathComponent("discover/movie"), resolvingAgainstBaseURL: false)!
+        
+        components.queryItems = [
+            URLQueryItem(name: "include_adult", value: "false"),
+            URLQueryItem(name: "include_video", value: "false"),
+            URLQueryItem(name: "language", value: "en_US"),
+            URLQueryItem(name: "page", value: String(page)),
+            URLQueryItem(name: "sort_by", value: "popularity.desc")
+        ]
+        
+        guard let url = components.url else {
+            throw FetchError.badResponse
+        }
+    }
 }
